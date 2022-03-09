7a037a8b6e312257d72bc57499b29833172414a7
Tom Adams <thomasjeadams@btinternet.com>
2022-03-09 11:16:47 +0000

    fix: add a cron job to send morning messages to all users

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const { App } = require("@slack/bolt")
 require('dotenv').config()
 const homeView = require('./homeView/index')
+const welcomeMessage = require('./messages/welcomeMessage')
+const CronJob = require('cron').CronJob
 
 const app = new App ({
     token: process.env.SLACK_BOT_TOKEN,
@@ -10,10 +12,36 @@ const app = new App ({
     port: process.env.PORT || 3000
 });
 
+// ------- GET USER IDs ------------
 
-app.message('Hello', async ({ message, say }) => {
-	await say("Good Bye");
-});
+const allUserIds = []
+const getAllUsersIds = async () => {
+	const allUsers = await app.client.users.list({'token':process.env.SLACK_BOT_TOKEN})
+	const allMembers = allUsers.members
+	allMembers.forEach(user => {
+		allUserIds.push(user.id)
+		console.log('user: ', user)
+	})
+	console.log('allUserIds: ', allUserIds)
+}
+
+getAllUsersIds()
+
+// ---------- MESSAGES ----------
+
+let job = new CronJob('0 28 9 * * *', async () => {
+	console.log("Message should be sent")
+	await allUserIds.forEach(userId => {
+		app.client.chat.postMessage({
+			token: process.env.SLACK_BOT_TOKEN,
+			channel: userId, 
+			text: welcomeMessage
+		})
+	})
+}, null, true, 'Europe/London')
+job.start()
+
+// --------- HOME ------------
 
 app.event('app_home_opened', async ({event, client, logger}) => {
 	try {
--- homeView/index.js
+++ homeView/index.js
@@ -8,8 +8,82 @@ await client.views.publish({
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": "Welcome"
+                    "text": "Welcome to Foodly! 🍔"
                 }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Feeling hungry?* \n \n Head over to 'Messages' to suggest somewhere to have lunch and to vote on other people's suggestions"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "🥇 *Podium* 🥇"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "1st - Person One (7 wins) \n 2nd - Person Two (5 wins) \n 3rd - Person Three (4 wins)"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "⚙️ *Settings*"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "When does voting close each day?"
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "timepicker",
+                        "initial_time": "12:00",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select time",
+                            "emoji": true
+                        },
+                        "action_id": "actionId-0"
+                    }
+                ]
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Save",
+                            "emoji": true
+                        },
+                        "value": "click_me_123",
+                        "action_id": "actionId-0"
+                    }
+                ]
             }
         ]
     }
