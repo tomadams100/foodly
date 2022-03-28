aa12818212aec8998d08dcd68668d87c174c95a2
Tom Adams <thomasjeadams@btinernet.com>
2022-03-28 18:24:48 +0100

    feat: move create new lunch location from main message to modal

--- messages/welcomeMessage.js
+++ messages/welcomeMessage.js
@@ -37,6 +37,15 @@ const welcomeMessage = async (app) => {
   const place_options = async () => {
     const workspace = await DB.getWorkspace();
     const workspace_suggestions = workspace.suggestions;
+    // if (typeof workspace_suggestions === 'undefined') {
+    //   await DB.updateWorkspace({
+    //     surveyId: today, field: 'suggestions', value:[{
+    //       placeName: 'Sample Place',
+    //       googleMaps: 'www.sampleplace.com',
+    //       tags:['Sample Tag']
+    //     }]
+    //   })
+    // }
     const survey = await DB.getSurvey({ surveyId: today });
     let survey_suggestions = [];
     if (survey.suggestions) {
@@ -47,15 +56,16 @@ const welcomeMessage = async (app) => {
 
     const options = diff(workspace_suggestions, survey_suggestions).map(
       (elem) => {
-        return {
-          text: {
-            type: "plain_text",
-            text: elem,
-            emoji: true,
-          },
-          value: elem,
-        };
-      }
+      console.log('elem', elem)  
+          return {
+            text: {
+              type: "plain_text",
+              text: elem.placeName,
+              emoji: true,
+            },
+            value: elem.placeName,
+          };
+        }
     );
     return options;
   };
@@ -119,50 +129,19 @@ const welcomeMessage = async (app) => {
         type: "divider",
       },
       {
-        type: "input",
-        dispatch_action: true,
-        element: {
-          action_id: "lunch_place_txt_box",
-          type: "plain_text_input",
-        },
-        label: {
-          type: "plain_text",
-          text: "Want to suggest somewhere new?",
-          emoji: true,
-        },
-      },
-      {
-        type: "input",
-        element: {
-          type: "plain_text_input",
-          action_id: "plain_text_input-action",
-          placeholder: {
-            type: "plain_text",
-            text: "Enter link...",
-          },
-        },
-        label: {
-          type: "plain_text",
-          text: "Google Maps link",
-          emoji: true,
-        },
-      },
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: "Tag your suggestion",
-        },
-        accessory: {
-          type: "multi_static_select",
-          placeholder: {
-            type: "plain_text",
-            text: "Select options",
-            emoji: true,
-          },
-          options: await place_options(),
-          action_id: "multi_static_select-action",
-        },
+        "type": "actions",
+        "elements": [
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Create New Suggestion",
+              "emoji": true
+            },
+            "value": "create_new_suggestion",
+            "action_id": "create_new_suggestion"
+          }
+        ]
       },
       {
         type: "divider",
--- messages/welcomeMessageActions.js
+++ messages/welcomeMessageActions.js
@@ -2,31 +2,214 @@ const DB = require("../DB");
 const updateMsgAllUsers = require("../foodlyFunctions/updateMsgAllUsers");
 const moment = require("moment");
 const today = moment().format("D.MM.YY");
+const util = require('util')
 
 module.exports = function (app) {
   app.action("lunch_place_txt_box", async (props) => {
     const { ack } = props;
     await ack();
-    const placeName = props.action.value;
-    console.log("You typed in the lunch place name text box:", placeName);
+    // const placeName = props.action.value;
+    // console.log("You typed in the lunch place name text box:", placeName);
+
+    // const workspace = await DB.getWorkspace();
+    // if (workspace.suggestions) {
+    //   await DB.updateWorkspace({
+    //     workspaceId: workspace.id,
+    //     field: "suggestions",
+    //     value: [...workspace.suggestions, placeName],
+    //   });
+    // } else {
+    //   await DB.updateWorkspace({
+    //     workspaceId: workspace.id,
+    //     field: "suggestions",
+    //     value: [placeName],
+    //   });
+    // }
+
+    // updateMsgAllUsers(app, DB);
+  });
+
+  app.action('tag_suggestions', async (props) => {
+    const { ack } = props
+    await ack()
+
+    //console.log('props.body.state.values.tag_block.tag_suggestion.selected_options', props.body.state.values.tag_block.tag_suggestion.selected_options)
+  })
+
+  app.action('create_new_suggestion', async (props) => {
+    const { ack, client, body } = props
+    await ack()
+
+    await client.views.open(
+      {
+        trigger_id: body.trigger_id,
+        view: {
+          type: 'modal',
+          callback_id: 'create_new_suggestion_modal_view',
+          title: {
+            type: 'plain_text',
+            text: 'Create New Suggestion'
+          },
+          submit: {
+            "type": "plain_text",
+             "text": "Submit"
+          },
+          blocks:[
+              // {
+              //   type: "input",
+              //   dispatch_action: true,
+              //   element: {
+              //     action_id: "lunch_place_txt_box",
+              //     type: "plain_text_input",
+              //   },
+              //   label: {
+              //     type: "plain_text",
+              //     text: "Place Name",
+              //     emoji: true,
+              //   }
+              // },
+              {
+                type: "input",
+                block_id: "place_name_block",
+                element: {
+                  type: "plain_text_input",
+                  action_id: "place_name_input",
+                  placeholder: {
+                    type: "plain_text",
+                    text: "Enter place name...",
+                  },
+                },
+                label: {
+                  type: "plain_text",
+                  text: "Place Name",
+                  emoji: true,
+                }
+              },
+              {
+                type: "input",
+                block_id: "google_maps_link_block",
+                element: {
+                  type: "plain_text_input",
+                  action_id: "google_maps_link",
+                  placeholder: {
+                    type: "plain_text",
+                    text: "Enter link...",
+                  },
+                },
+                label: {
+                  type: "plain_text",
+                  text: "Google Maps link",
+                  emoji: true,
+                }
+              },
+              {
+                type: "section",
+                block_id: "tags_block",
+                text: {
+                  type: "mrkdwn",
+                  text: "Tag your suggestion",
+                },
+                accessory: {
+                  type: "multi_static_select",
+                  placeholder: {
+                    type: "plain_text",
+                    text: "Select options",
+                    emoji: true,
+                  },
+                  options: [
+                    {
+                      "text": {
+                        "type": "plain_text",
+                        "text": "Italian",
+                        "emoji": true
+                      },
+                      "value": "Italian"
+                    },
+                    {
+                      "text": {
+                        "type": "plain_text",
+                        "text": "Asian",
+                        "emoji": true
+                      },
+                      "value": "Asian"
+                    },
+                    {
+                      "text": {
+                        "type": "plain_text",
+                        "text": "Mexican",
+                        "emoji": true
+                      },
+                      "value": "Mexican"
+                    },
+                    {
+                      "text": {
+                        "type": "plain_text",
+                        "text": "Relaxed",
+                        "emoji": true
+                      },
+                      "value": "Relaxed"
+                    },
+                    {
+                      "text": {
+                        "type": "plain_text",
+                        "text": "Take-away",
+                        "emoji": true
+                      },
+                      "value": "Take-away"
+                    },
+                    {
+                      "text": {
+                        "type": "plain_text",
+                        "text": "Fancy",
+                        "emoji": true
+                      },
+                      "value": "Fancy"
+                    },
+                    {
+                      "text": {
+                        "type": "plain_text",
+                        "text": "Good Value",
+                        "emoji": true
+                      },
+                      "value": "Good Value"
+                    }
+                  ],
+                  action_id: "tag_suggestions",
+                }
+              }
+          ]
+      }
+    })
+  })
+
+  app.view('create_new_suggestion_modal_view', async (props) => {
+    const { ack, body, view, client } = props
+    ack()
+    const tags = view.state.values.tags_block.tag_suggestions.selected_options.map(elem => elem.value)
+    const suggestion = {
+          placeName: view.state.values.place_name_block.place_name_input.value,
+          googleMaps: view.state.values.google_maps_link_block.google_maps_link.value,
+          tags: tags
+        };
+    console.log('You tried to submit this to the workplace DB:', util.inspect(suggestion))
 
     const workspace = await DB.getWorkspace();
     if (workspace.suggestions) {
       await DB.updateWorkspace({
         workspaceId: workspace.id,
         field: "suggestions",
-        value: [...workspace.suggestions, placeName],
+        value: [...workspace.suggestions, suggestion],
       });
     } else {
       await DB.updateWorkspace({
         workspaceId: workspace.id,
         field: "suggestions",
-        value: [placeName],
+        value: [suggestion],
       });
     }
 
     updateMsgAllUsers(app, DB);
-  });
+  })
 
   app.action("click_submit_suggestion", async (props) => {
     const { ack, body } = props;
